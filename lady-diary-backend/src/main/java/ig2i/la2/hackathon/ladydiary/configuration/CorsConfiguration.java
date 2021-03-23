5d104c04d9281a726893231e186704aee28ef66e
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-03-23 15:18:17 +0100

    CORS

--- lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/configuration/CorsConfiguration.java
+++ lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/configuration/CorsConfiguration.java
@@ -2,9 +2,11 @@ package ig2i.la2.hackathon.ladydiary.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
