6c10fdce5fb4c80ca8af73da95c9c9059a394285
CaronMartin <martin.caron@ig2i.centralelille.fr>
2021-03-23 13:50:00 +0100

    CRUD Topic

--- lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/domain/topic/TopicNotFoundException.java
+++ lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/domain/topic/TopicNotFoundException.java
@@ -4,8 +4,8 @@ import ig2i.la2.hackathon.ladydiary.domain.erros.NotFoundException;
 
 public class TopicNotFoundException extends NotFoundException {
 
-    private static final String error = "record.notFound";
-    private static final String message = "This record can not be found";
+    private static final String error = "topic.notFound";
+    private static final String message = "This topic can not be found";
 
     public TopicNotFoundException(){
         super(error, message);
