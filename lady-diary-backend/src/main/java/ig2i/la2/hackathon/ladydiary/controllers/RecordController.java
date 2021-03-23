d921d82aa26af0d1605cb385262cca2b269b66d1
Sacha <sacha.lesueur@ig2i.centralelille.fr>
2021-03-23 13:15:41 +0100

    crud records

--- lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/controllers/RecordController.java
+++ lady-diary-backend/src/main/java/ig2i/la2/hackathon/ladydiary/controllers/RecordController.java
@@ -1,6 +1,8 @@
 package ig2i.la2.hackathon.ladydiary.controllers;
 
+import ig2i.la2.hackathon.ladydiary.domain.erros.WrongFormatException;
 import ig2i.la2.hackathon.ladydiary.domain.record.Record;
+import ig2i.la2.hackathon.ladydiary.domain.record.RecordNotFoundException;
 import ig2i.la2.hackathon.ladydiary.services.RecordService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -8,14 +10,12 @@ import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/records")
+@RequestMapping("/v1/records")
 @RequiredArgsConstructor
 public class RecordController {
 
@@ -37,4 +37,28 @@ public class RecordController {
         }
     }
 
+    @PostMapping()
+    public ResponseEntity<HttpStatus> createRecord(@RequestBody Record record) throws WrongFormatException {
+        recordService.createRecord(record);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{idRecord}")
+    public ResponseEntity<HttpStatus> deleteRecordById(@PathVariable int idRecord) throws RecordNotFoundException {
+        recordService.deleteRecord(idRecord);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping()
+    public ResponseEntity<HttpStatus> updateRecord(@RequestBody Record record) throws RecordNotFoundException{
+        recordService.updateRecord(record);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/{idRecord}")
+    public ResponseEntity<Record> getRecordById(@PathVariable int idRecord) throws RecordNotFoundException{
+        Record record = recordService.findRecordById(idRecord);
+        return ResponseEntity.status(HttpStatus.OK).body(record);
+    }
 }
+
