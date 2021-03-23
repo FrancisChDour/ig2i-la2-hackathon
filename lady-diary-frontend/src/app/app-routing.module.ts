b5af5de0d8f584084eff26d9df279e4768517488
GuillaumeCar <37478102+GuillaumeCar@users.noreply.github.com>
2021-03-23 14:09:56 +0100

    route + component

--- lady-diary-frontend/src/app/app-routing.module.ts
+++ lady-diary-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TopicComponent} from "./topic/topic.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'topics', component: TopicComponent},
+  {path: '', redirectTo: '/topics', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
